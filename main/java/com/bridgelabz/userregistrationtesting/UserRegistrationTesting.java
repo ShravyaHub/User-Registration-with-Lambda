56e73b9d05b6f236e3069982ee4f6073e8a0b3a7
ShravyaHubb <shravyak146@gmail.com>
2020-10-25 17:18:54 +0530

    Modified file: UserRegistrationTesting.java to test email using JUnit parameterized Testing

--- main/java/com/bridgelabz/userregistrationtesting/UserRegistrationTesting.java
+++ main/java/com/bridgelabz/userregistrationtesting/UserRegistrationTesting.java
@@ -5,6 +5,7 @@ public class UserRegistrationTesting {
     public static final String NAME_REGEX = "^[A-Z][a-z]{2,}$";
     public static final String PHONE_NUMBER_REGEX = "^[9][1][ ][6-9][0-9]{9}$";
     public static final String PASSWORD_REGEX = "^(?=.*[!@#$%^&+=])(?=.*[0-9])(?=.*[A-Z]).{8,}$";
+    public static final String EMAIL_REGEX = "^([a-z0-9]+[-._+]?[a-z0-9]+)+@[a-z0-9-]+.[a-z]{2,3}.[a-z]{2,3}$";
 
     public boolean validateFirstName(String firstName) {
         return firstName.matches(NAME_REGEX);
@@ -20,6 +21,10 @@ public class UserRegistrationTesting {
 
     public boolean validatePassword(String password) {
         return password.matches(PASSWORD_REGEX);
+    }
+
+    public boolean validateEmail(String email) {
+        return email.matches(EMAIL_REGEX);
     }
 
 }
