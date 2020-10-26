7129ee1e16dcae1fc41c0c565084c01ef5faec4e
ShravyaHubb <shravyak146@gmail.com>
2020-10-26 11:43:14 +0530

    Refactored file: UserRegistrationTesting.java to validate user details with lambda

--- main/java/com/bridgelabz/userregistrationtesting/UserRegistrationTesting.java
+++ main/java/com/bridgelabz/userregistrationtesting/UserRegistrationTesting.java
@@ -1,30 +1,41 @@
 package com.bridgelabz.userregistrationtesting;
 
+@FunctionalInterface
+interface IValidateFunction {
+    boolean validate(String credential, String pattern);
+}
+
 public class UserRegistrationTesting {
 
     public static final String NAME_REGEX = "^[A-Z][a-z]{2,}$";
     public static final String PHONE_NUMBER_REGEX = "^[9][1][ ][6-9][0-9]{9}$";
     public static final String PASSWORD_REGEX = "^(?=.*[!@#$%^&+=])(?=.*[0-9])(?=.*[A-Z]).{8,}$";
     public static final String EMAIL_REGEX = "^([a-z0-9]+[-._+]?[a-z0-9]+)+@[a-z0-9-]+.[a-z]{2,3}.[a-z]{2,3}$";
+    IValidateFunction iValidateFunction;
+
+    public UserRegistrationTesting()
+    {
+        iValidateFunction = (credential , pattern) -> credential.matches(pattern);
+    }
 
     public boolean validateFirstName(String firstName) {
-        return firstName.matches(NAME_REGEX);
+        return iValidateFunction.validate(firstName, NAME_REGEX);
     }
 
     public boolean validateLastName(String lastName) {
-        return lastName.matches(NAME_REGEX);
+        return iValidateFunction.validate(lastName, NAME_REGEX);
     }
 
     public boolean validatePhoneNumber(String phoneNumber) {
-        return phoneNumber.matches(PHONE_NUMBER_REGEX);
+        return iValidateFunction.validate(phoneNumber, PHONE_NUMBER_REGEX);
     }
 
     public boolean validatePassword(String password) {
-        return password.matches(PASSWORD_REGEX);
+        return iValidateFunction.validate(password, PASSWORD_REGEX);
     }
 
     public boolean validateEmail(String email) {
-        return email.matches(EMAIL_REGEX);
+        return iValidateFunction.validate(email, EMAIL_REGEX);
     }
 
 }
