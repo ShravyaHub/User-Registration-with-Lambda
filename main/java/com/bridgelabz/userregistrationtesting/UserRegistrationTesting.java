b30eceea6b7288cef1984efdd9db1cc3eb31e1f6
ShravyaHubb <shravyak146@gmail.com>
2020-10-27 10:44:21 +0530

    ADDED: added code to validate user registration with predicate

--- main/java/com/bridgelabz/userregistrationtesting/UserRegistrationTesting.java
+++ main/java/com/bridgelabz/userregistrationtesting/UserRegistrationTesting.java
@@ -1,5 +1,8 @@
 package com.bridgelabz.userregistrationtesting;
 
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
 @FunctionalInterface
 interface IValidateFunction {
     boolean validate(String credential, String pattern);
@@ -19,22 +22,62 @@ public class UserRegistrationTesting {
     }
 
     public boolean validateFirstName(String firstName) {
+        Pattern pattern = Pattern.compile(NAME_REGEX);
+        Predicate<String> predicate = pattern.asPredicate();
+        boolean result = predicate.test(firstName);
+        if(result) {
+            System.out.println("First name is valid");
+        } else {
+            System.out.print("First name is invalid");
+        }
         return iValidateFunction.validate(firstName, NAME_REGEX);
     }
 
     public boolean validateLastName(String lastName) {
+        Pattern pattern = Pattern.compile(NAME_REGEX);
+        Predicate<String> predicate = pattern.asPredicate();
+        boolean result = predicate.test(lastName);
+        if(result) {
+            System.out.println("Last name is valid");
+        } else {
+            System.out.print("Last name is invalid");
+        }
         return iValidateFunction.validate(lastName, NAME_REGEX);
     }
 
     public boolean validatePhoneNumber(String phoneNumber) {
+        Pattern pattern = Pattern.compile(PHONE_NUMBER_REGEX);
+        Predicate<String> predicate = pattern.asPredicate();
+        boolean result = predicate.test(phoneNumber);
+        if(result) {
+            System.out.println("Phone number is valid");
+        } else {
+            System.out.print("Phone number is invalid");
+        }
         return iValidateFunction.validate(phoneNumber, PHONE_NUMBER_REGEX);
     }
 
     public boolean validatePassword(String password) {
+        Pattern pattern = Pattern.compile(PASSWORD_REGEX);
+        Predicate<String> predicate = pattern.asPredicate();
+        boolean result = predicate.test(password);
+        if(result) {
+            System.out.println("Password is valid");
+        } else {
+            System.out.print("Password is invalid");
+        }
         return iValidateFunction.validate(password, PASSWORD_REGEX);
     }
 
     public boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Predicate<String> predicate = pattern.asPredicate();
+        boolean result = predicate.test(email);
+        if(result) {
+            System.out.println("Email is valid");
+        } else {
+            System.out.print("Email is invalid");
+        }
         return iValidateFunction.validate(email, EMAIL_REGEX);
     }
 
